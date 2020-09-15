e2c90b22950d6e66a2c251d6f5dd3125f418a0dc
tranhaoTNH <hao.tn@beetechsoft.vn>
2020-09-15 21:42:17 +0700

    Delete File .history

--- .history/src/features/home/Home_20200824153245.js
+++
@@ -1,8 +0,0 @@
-import React, { useEffect } from "react";
-// import { getCategories, postAccount, verifyEmail } from "./actions";
-
-const Home = () => {
-  return <div>Home Page</div>;
-};
-
-export default Home;
--- .history/src/utils/redux/rootReducers_20200808103418.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-
-import systemReducer from '../../features/system/reducer';
-
-const rootReducer = combineReducers({
-  systemReducer,
-});
-
-export default rootReducer;
